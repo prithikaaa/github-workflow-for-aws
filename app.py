f1e87026e916ff78b6fb3474727f3ccae276a274
Prithika <titlidatt1000@gmail.com>
2026-06-09 14:20:44 +0530

    changed flask response

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from GitHub Actions! yes"
+    return "Hello from GitHub Actions! This is Prithika."
 
 if __name__ == '__main__':
     app.run()
