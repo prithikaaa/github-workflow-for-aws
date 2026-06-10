0c24b6252303ab22b3222646764785a83306701b
Prithika <titlidatt1000@gmail.com>
2026-06-10 15:07:47 +0530

    output change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from GitHub Actions! This is Prithika."
+    return "Hello from GitHub Actions! This is Prithika. This is CI-CD version 2.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
